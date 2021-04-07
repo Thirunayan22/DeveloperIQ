14fd6eeb15dfb0c5f59b464e22c680c6402ee03e
Thirunayan22 <thirunayan22@gmail.com>
2021-04-07 23:05:25 +0530

    added commit info sorting and returning

--- DBUpdateService/github_data_api.py
+++ DBUpdateService/github_data_api.py
@@ -1,14 +1,17 @@
 import os
 import json
 import fastapi
-from typing import List,Optional,Set,Dict
+from typing import List, Optional, Set, Dict
 from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
 import uvicorn
 import zulu
 import random
-from datetime import datetime
+import datetime
+import dateutil
+import dateutil.relativedelta
+
 """
 For a given username this API should be able to return all information needed for the calculation of the metric for 
 every user
@@ -30,18 +33,19 @@ app = FastAPI()
 #######
 CONTRIBUTOR_COMMITS_API_URI = "https://api.github.com/repos/RasaHQ/rasa/commits"
 CONTRIBUTOR_PULL_REQUESTS_API_URI = "https://api.github.com/repos/RasaHQ/rasa/pulls"
-CONTRIBUTOR_ISSUES_API_URI ="https://api.github.com/repos/RasaHQ/rasa/issues"
+CONTRIBUTOR_ISSUES_API_URI = "https://api.github.com/repos/RasaHQ/rasa/issues"
 COLLABORATOR_API_URI = "https://api.github.com/repos/RasaHQ/rasa/contributors"
 
 
 # TODO: Change code to not include contributor name in every decorated parameter call
 # Contributor Attributes
 class ContributorAttr(BaseModel):
-    contributor_name: str
-    time_frame: str   # "monthly","weekly","daily"
+    contributor_login: str
+    time_frame: str  # "monthly","weekly","daily"
 
-@app.post("/contributor/commits")
-async def get_contributer_commit_count(contributor:ContributorAttr,time_frame:str):
+
+@app.post("/contributor/snapshot")
+def get_contributer_commit_count(contributor: ContributorAttr, time_frame: str):
     """
     Gets number of commits made by contributor
 
@@ -49,66 +53,63 @@ async def get_contributer_commit_count(contributor:ContributorAttr,time_frame:st
     ==============
 
     ## SHOULD BE POSSIBLE TO RETURN BY EACH TIME FRAME (WEEK,DAY,MONTH)
-    contributor_name:{
-    "commits":20,
-    "additions": 325
-    }
 
+    @:returns
+   DICT:{"commits":20,
+    "total_contribution": 325}
     """
-    # time_frame       = contributor.time_frame
-    #TODO FILTER BY TIME RANGE
-    # GET TOTAL NUMBER OF ADDITIONS BY QUERYING FOR THE SPECIFIC COMMIT
-    # (https://docs.github.com/en/rest/reference/repos#get-a-commit)
-
-##Sort by commits per day , commits per week
-
-    # commits_per_day = {}
-    # prev_date  = 0
-    # temp_num_commit = 0
     since = None
-    present_time = zulu.now().isoformat().split(".")[0]
+    commit_info = {}
     if time_frame == "year":
-        since = f"{int(present_time.split('-')[0])-1}-{present_time.split('-')[1]}-{present_time.split('-')[2]}"
+        since = str(zulu.parse(datetime.datetime.now() - dateutil.relativedelta.relativedelta(years=1))).split('.')[0]
     elif time_frame == "month":
-        since = f"{present_time.split('-')[0]}-{int(present_time.split('-')[1])-1}-{present_time.split('-')[2]}"
+        since = str(zulu.parse(datetime.datetime.now() - dateutil.relativedelta.relativedelta(months=1))).split('.')[0]
     elif time_frame == "day":
-        since = f"{present_time.split('-')[0]}-{present_time.split('-')[1]}-{int(present_time.split('-')[2].split('T')[0])-1}T{present_time.split('-')[2].split('T')[1]}"
+        since = str(zulu.parse(datetime.datetime.now() - dateutil.relativedelta.relativedelta(days=1))).split('.')[0]
+    elif time_frame == "week":
+        since = str(zulu.parse(datetime.datetime.now() - dateutil.relativedelta.relativedelta(weeks=1))).split('.')[0]
+
+    contributor_login = contributor.contributor_login
+    url = f"{CONTRIBUTOR_COMMITS_API_URI}?author={contributor_login}&since={since}"
     print(since)
-    contributor_name = contributor.contributor_name
-    url = f"{CONTRIBUTOR_COMMITS_API_URI}?author={contributor_name}&since={since}"
-    raw_contributor_commits =  requests.request("GET",url).json()
+    raw_contributor_commits = requests.request("GET", url).json()
+    print(raw_contributor_commits)
+    commit_info["contributor_login"] = contributor_login
+    commit_info["contributor_username"] = raw_contributor_commits[0]["commit"]["author"]["name"]
+    commit_info["num_commits"] = len(raw_contributor_commits)
+    commit_info["time_frame"] = time_frame
+    commit_info["commit_additions"] = 0
+    commit_info["commit_deletions"] = 0
+
+    for commit_idx in range(len(raw_contributor_commits)):
+        curr_commit = raw_contributor_commits[commit_idx]
+        commit_ref = curr_commit["sha"]
+        commit_request_url = f"{CONTRIBUTOR_COMMITS_API_URI}/{commit_ref}"
+        commit_info_request = requests.request("GET",commit_request_url).json()
+        print(commit_info_request)
+        commit_additions = commit_info_request["stats"]["additions"]
+        commit_deletions = commit_info_request["stats"]["deletions"]
+
+        commit_info["commit_additions"] += commit_additions
+        commit_info["commit_deletions"] += commit_deletions
+
+    return commit_info
 
 
-
-    # for commit_idx in range(len(raw_contributor_commits)):
-    #     current_commit = raw_contributor_commits[commit_idx]
-    #
-    #     commit_time_stamp = current_commit["commit"]["committer"]["date"]
-    #     # commit_message = current_commit["commit"]["message"] : OPTIONAL
-    #     commit_date = str(zulu.parse(commit_time_stamp).datetime.date())
-    #     if commit_date != prev_date:
-    #         temp_num_commit = 1
-    #         commits_per_day[commit_date] = temp_num_commit
-    #     else:
-    #         temp_num_commit += 1
-    #         commits_per_day[commit_date] = temp_num_commit
-    #     prev_date = commit_date
-
-    return raw_contributor_commits
-
-
-@app.post("/{contributor_name}/pull-requests")
-async def get_contributor_pull_requests(contributor_name:str,created:bool,reviewed:bool):
+@app.post("/{contributor_login}/pull-requests")
+async def get_contributor_pull_requests(contributor_name: str, created: bool, reviewed: bool):
     """" Return Pull Requests from contributor """
-    #TODO : ADD RETURN FORMAT
+    # TODO : ADD RETURN FORMAT
     return "reponse:200"
 
-@app.post("/{contributor_name}/issue")
-async def get_contributor_issues(contributor_name:str,created:bool,resolved:bool):
+
+@app.post("/{contributor_login}/issue")
+async def get_contributor_issues(contributor_name: str, created: bool, resolved: bool):
     """" Return issues created by contributer and issues resolved """
-    #TODO : ADD RETURN FORMAT
+    # TODO : ADD RETURN FORMAT
 
     return "response:200"
 
+
 if __name__ == "__main__":
-    uvicorn.run(app,host="0.0.0.0",port=8001)
+    uvicorn.run("github_data_api:app", host="127.0.0.1", port=8001, reload=True)
